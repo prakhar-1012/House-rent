cd0d159e12ea5393f4244395305481455da9efb3
prakhar-1012 <129137020+prakhar-1012@users.noreply.github.com>
2024-03-25 11:43:53 +0530

    Update house_rent.py

--- house_rent.py
+++ house_rent.py
@@ -4,3 +4,7 @@ import tensorflow_decision_forests as tfdf
 import matplotlib.pyplot as plt
 import seaborn as sns
 %matplotlib inline
+
+print("TensorFlow v" + tf.__version__)
+print("TensorFlow Decision Forests v" + tfdf.__version__)
+
